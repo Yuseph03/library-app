df6315a493233e44b179ced5f48a349e89cfd7e4
Yuseph <vernonnashville@gmail.com>
2023-02-09 12:56:50 +0330

    Add initial files and linting dependencies

---
+++ sccript.js
@@ -0,0 +1,12 @@
+const mylibrary = [];
+
+function Book(author, title, pages, readStatus) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+Book.prototype.addBook = function () {
+  mylibrary.push();
+};
