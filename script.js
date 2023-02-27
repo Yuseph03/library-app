8629d9fa7ebcd75d3b94d876896f48b7128f9b58
Yuseph <vernonnashville@gmail.com>
2023-02-27 09:30:12 +0330

    Add footer and font.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const myLibrary = [];
+let myLibrary = [];
 const addBookBtn = document.getElementById('add-book');
 const bookForm = document.getElementById('myForm');
 const pageMask = document.getElementById('page-mask');
 const cardContainer = document.getElementById('card-container');
-const readToggleBtns = document.querySelectorAll('.readToggle');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -22,11 +21,26 @@ function openForm() {
 }
 
 function closeForm() {
+  const doubleError = document.querySelector('.doubleError');
   bookForm.style.display = 'none';
   pageMask.style.display = 'none';
+  doubleError.remove();
 }
 
-function displayBookCard(newBook) {
+function changeReadStatus(el, classname) {
+  if (el.classList.contains(classname)) {
+    el.classList.remove(classname);
+  } else {
+    el.classList.add(classname);
+  }
+}
+
+function removeBookCard(e, book) {
+  e.parentElement.remove();
+  myLibrary = myLibrary.filter((el) => el.title !== book.title);
+}
+
+function addBookCard(newBook) {
   const bookReadStatus = document.getElementById('readStatus').checked;
   const bookCard = document.createElement('div');
   const bookTitle = document.createElement('p');
@@ -38,12 +52,14 @@ function displayBookCard(newBook) {
   bookCard.classList.add('card');
   readBtn.setAttribute('id', 'readToggle');
   readBtn.classList.add('readToggle');
-  rmvBtn.setAttribute('id', 'removeBook');
   bookTitle.innerText = newBook.title;
   bookAuthor.innerText = newBook.author;
   bookPages.innerText = newBook.pages;
   readBtn.textContent = 'Read';
   rmvBtn.textContent = 'Remove';
+  rmvBtn.setAttribute('id', 'removeBook');
+  rmvBtn.addEventListener('click', (e) => { removeBookCard(e.target, newBook); });
+  readBtn.addEventListener('click', (e) => { changeReadStatus(e.target, 'on'); });
 
   if (bookReadStatus === true) {
     readBtn.classList.add('on');
@@ -72,16 +88,14 @@ function addToLibrary(e) {
 
   if (!isInLibrary(newBook)) {
     myLibrary.push(newBook);
-    displayBookCard(newBook);
-  }
-  // TODO: duplicate Error
-}
-
-function changeReadStatus(el, classname) {
-  if (el.classList.contains(classname)) {
-    el.classList.remove(classname);
+    addBookCard(newBook);
+    closeForm();
   } else {
-    el.classList.add(classname);
+    const doubleError = document.createElement('p');
+    doubleError.textContent = 'This book already exists in your Library!';
+    doubleError.style.color = 'red';
+    doubleError.classList.add('doubleError');
+    bookForm.append(doubleError);
   }
 }
 
@@ -90,7 +104,3 @@ document.getElementById('addBookSubmit').addEventListener('click', addToLibrary)
 addBookBtn.addEventListener('click', openForm);
 
 pageMask.addEventListener('click', closeForm);
-
-readToggleBtns.forEach((button) => {
-  button.addEventListener('click', (e) => { changeReadStatus(e.target, 'on'); });
-});
